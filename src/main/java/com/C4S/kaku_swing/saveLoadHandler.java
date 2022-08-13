7f77b28f120d4ba6d446b3a2c13f72c431689079
James Koehler <jameskoehler1212@gmail.com>
2022-08-12 21:37:29 -0400

    Updates to saving
    There are now two methods used for saving. One for saving games, and one for settings. The game save one may need some tweaking, and has not yet been implemented into anything else.

--- src/main/java/com/C4S/kaku_swing/saveLoadHandler.java
+++ src/main/java/com/C4S/kaku_swing/saveLoadHandler.java
@@ -14,7 +14,56 @@ import net.harawata.appdirs.AppDirsFactory;
 
 public class saveLoadHandler {
 
-    public static void writeToSaveFile(String index, String  value){
+    public static void writeToSaveFile(String index, String value){
+
+        /*
+        Save format will have a saved game as the key, and the moves made saved in PGN. The engine needs to be able to store all moves made and convert to and from PGN.
+        Example:
+        "Game 1": "1.Nf3 Nf6 2.c4 g6 3.Nc3 Bg7 4.d4 O-O 5.Bf4 d5 6.Qb3 dxc4 7.Qxc4 c6 8.e4 Nbd7",
+        "Game 2": "1.Nf3 Nf6 2.c4 g6 3.Nc3 Bg7 4.d4 O-O 5.Bf4 d5 6.Qb3 dxc4 7.Qxc4 c6 8.e4 Nbd7" <- imagine these games don't have the same moves made. I am lazy.
+         */
+
+        JSONParser parser = new JSONParser();
+        JSONObject obj = new JSONObject();
+
+        AppDirs appDirs = AppDirsFactory.getInstance();
+
+        try(FileReader reader = new FileReader(appDirs.getUserDataDir("kaku-swing",null,"C4 Software") + "/save.json")){
+
+            obj = (JSONObject)parser.parse(reader);
+        }catch(FileNotFoundException e){
+
+            System.out.println("No save.json file was found. Creating one now...");
+
+            File theDir = new File(appDirs.getUserDataDir("kaku-swing",null,"C4 Software"));
+
+            if(!theDir.exists())
+                theDir.mkdirs();
+
+            try(FileWriter file = new FileWriter(appDirs.getUserDataDir("kaku-swing",null,"C4 Software") + "/save.json")){
+
+                file.write(obj.toJSONString());
+            } catch (Exception ee){
+
+                System.out.println("Something went wrong when generating a new save.json.");
+            }
+        } catch (Exception e ){
+
+            System.out.println("An unknown error occurred when reading save.json.");
+        }
+
+        obj.put(index, value);
+
+        try(FileWriter file = new FileWriter(appDirs.getUserDataDir("kaku-swing",null,"C4 Software") + "/save.json")){
+
+            file.write(obj.toJSONString());
+        } catch (Exception ee){
+
+            System.out.println("Something went wrong when saving changes to save.json.");
+        }
+    }
+
+    public static void writeToSettingsFile(String index, String value){
 
         JSONParser parser = new JSONParser();
         JSONObject obj = new JSONObject();
